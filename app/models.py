72f6b8ffea981565814a411ae7287f00770a9cd1
Nikita Syulin <nikita_sulin@idpowers.com>
2022-02-16 17:39:57 +0700

    models and schemas

--- app/models.py
+++ app/models.py
@@ -1,10 +1,72 @@
-from sqlalchemy import Column, String, Float, Integer
+from sqlalchemy import Column, String, Float, Integer, ForeignKey, DateTime, Date, Table
 from database import Base
+from sqlalchemy.orm import relationship
 
 
-class Product(Base):
+class BaseIndex(Base):
+    id = Column(Integer, primary_key=True, index=True)
+
+
+class BaseName(Base):
+    name = Column(String)
+
+
+class Product(BaseIndex, BaseName):
     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
     current_volume = Column(Float)
+
+
+class Batch(BaseIndex):
+    __tablename__ = 'batches'
+
+    product = Column(Integer, ForeignKey('products.id'))
+    number = Column(Integer)
+    date_time = Column(DateTime)
+    weight = Column(Float)
+    supplier = Column(String)
+    quantity = Column(Float)
+    tank = Column(Integer, ForeignKey('tanks.id'))
+    density = Column(Float)  # read-only
+    received_shift = Column(String)
+
+
+class Tank(BaseIndex, BaseName):
+    __tablename__ = 'tanks'
+
+    capacity = Column(Float)
+
+
+class Supplier(BaseIndex, BaseName):
+    __tablename__ = 'suppliers'
+
+
+class Employee(BaseIndex):
+    __tablename__ = 'employees'
+
+    full_name = Column(String)
+    date_of_employment = Column(Date)
+    date_of_layoff = Column(Date)
+    sex = Column(String)  # Make choices?
+    experience = Column(Integer)  # read-only
+    date_of_birth = Column(Date)
+
+
+class Trade(BaseIndex):
+    __tablename__ = 'trades'
+
+    total = Column(Float)
+    date_time = Column(DateTime)
+    shift = Column(Integer, ForeignKey('shifts.id'))
+
+
+shift_employee = Table('shift_employee', Base.metadata,
+                       Column('shift_id', ForeignKey('shifts.id'), primary_key=True),
+                       Column('employee_id', ForeignKey('employees.id'), primary_key=True)
+                       )
+
+
+class Shift(BaseIndex):
+    __tablename__ = 'shifts'
+
+    employees = relationship('Employee', secondary='shift_employee', back_populates='shifts')
--- app/schemas.py
+++ app/schemas.py
@@ -1,21 +1,56 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime, date
 
 
-class Product(BaseModel):
+class BaseIndex(BaseModel):
+    id: int = Field(primary_key=True, index=True)
+
+
+class BaseName(BaseModel):
     name: str
+
+
+class Product(BaseName, BaseIndex):
     current_volume: float
 
     class Config:
         orm_mode = True
 
 
-# class Batch(BaseModel):
-#     product: fk
-#     number: int
-#     date_time: datetime
-#     weight: float
-#     supplier: str
-#     quantity: float
-#     tank: fk
-#     density: float(read_only=True)
-#     received_shift: str
+class Batch(BaseIndex):
+    product: int
+    number: int
+    date_time: datetime
+    weight: float
+    supplier: str
+    quantity: float
+    tank: int
+    density: float
+    received_shift: str
+
+
+class Tank(BaseIndex, BaseName):
+    capacity: float
+
+
+class Supplier(BaseIndex, BaseName):
+    pass
+
+
+class Employee(BaseIndex):
+    full_name: str
+    date_of_employment: date
+    date_of_layoff: date
+    sex: str  # Make choices?
+    experience: int  # read-only
+    date_of_birth: date
+
+
+class Trade(BaseIndex):
+    total: float
+    date_time: datetime
+    shift: int
+
+
+class Shift(BaseIndex):
+    employees: list[int, ]
