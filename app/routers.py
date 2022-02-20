bd391fe63002c032adc20f418391c13e6faf4317
Nikita Syulin <nikita_sulin@idpowers.com>
2022-02-20 17:55:56 +0700

    fix create employee response bug

--- app/routers.py
+++ app/routers.py
@@ -35,10 +35,11 @@ async def read_employee(employee_id: int, db: Session = Depends(get_db)):
 
 
 @router.post('/employees', tags=['employees'], response_model=schemas.Employee)
-async def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
+async def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)) -> schemas.Employee:
     db_employee = crud.get_employee(db, 100)
     if db_employee:
         raise HTTPException(400, detail='Employee already exists')
     response = crud.new_employee(db, employee)
-    print('\n', response)
-    return {'status': 200}
+
+    # pydantic requires response to be a dictionary
+    return response.__dict__
